e6ac8660a4c3f991c768f321fb4240ec887ea940
Dickson Karis <dicksonkariuki4@gmail.com>
2019-09-29 09:22:25 +0300

    Add constants class

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { User } from "../user";
+import { UserService } from "../users/user.service";
+import { AlertsService } from "../alert-service/alerts.service";
+import { Repository } from "../repository-class/repository";
+import { HttpClient } from "@angular/common/http";
+import { checkAndUpdateBinding } from "@angular/core/src/view/util";
+import { environment } from "../../environments/environment";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  providers: [UserService], //add the providers to the component
+  styleUrls: ["./user.component.css"]
 })
 export class UserComponent implements OnInit {
+  // users = [
+  //new User(""),
+  //]
+  //addNewUser(user){
+  //this.users.push(user)
+  //}
+  username: User;
+  repos: Repository;
 
-  constructor() { }
-
-  ngOnInit() {
+  check(rep) {
+    interface ApiResponse {
+      login: string;
+      public_repos: number;
+      followers: number;
+      following: number;
+      created_at: Date;
+      html_url: string;
+      avatar_url: string;
+    }
+    this.http
+      .get<ApiResponse>(
+        "https://api.github.com/users/" +
+          rep.username +
+          "?access_token=" +
+          "7377647d064385d967e03bfbffdc62983c19341a"
+      )
+      .subscribe(data => {
+        this.repos.login = data.login;
+        this.repos.public_repos = data.public_repos;
+        this.repos.followers = data.followers;
+        this.repos.created_at = data.created_at;
+        this.repos.html_url = data.html_url;
+        this.repos.avatar_url = data.avatar_url;
+      });
+    console.log(this.repos);
+  }
+  constructor(private http: HttpClient) {
+    this.repos = new Repository("", 0, 0, 0, new Date(), "", "");
   }
 
+  ngOnInit() {}
 }
